4205b57e0df9333be47f8f3d35a9bd891e5e13e5
Dilan Alvaro Alavi Gutierrez <dilanalavi11@gmail.com>
2023-09-05 10:25:02 -0400

    Ingresamo valor

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,15 @@
-import sumar from "./sumador";
+import password from "./app.js";
 
 const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
+
 form.addEventListener("submit", (event) => {
+  const aux=new password;
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const result= aux.ingresarDatos(firstNumber);
+  div.innerHTML = "<p>" + result + "</p>";
 });
